dcfad7b76afac381f4973199e99bafb99e253637
Linjiajie2002 <jiajielin2002@gmail.com>
2025-10-06 11:29:18 +0200

    JJL-UPLOAD

--- Delivery1/Assets/_Scripts/Connect.cs
+++ Delivery1/Assets/_Scripts/Connect.cs
@@ -1,10 +1,47 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class Connect : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Simulator simulator;
+
+    void OnEnable()
+    {
+       
+        Simulator.OnNewPlayer += OnPlayerCreated;
+    }
+
+    void OnDisable()
+    {
+       
+        Simulator.OnNewPlayer -= OnPlayerCreated;
+    }
+
+   
+    void OnPlayerCreated(string name, string country, int age, float gender, DateTime date)
+    {
+        Debug.Log($"Name: {name}");
+        Debug.Log($"Country: {country}");
+        Debug.Log($"Age: {age}");
+        Debug.Log($"Gender (float): {gender}");
+        Debug.Log($"Date: {date.ToString("yyyy-MM-dd HH:mm:ss")}");
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("user", name));
+        formData.Add(new MultipartFormDataSection("country", country));
+        formData.Add(new MultipartFormDataSection("age", age.ToString()));
+        formData.Add(new MultipartFormDataSection("gender", gender.ToString()));
+        formData.Add(new MultipartFormDataSection("register_date", date.ToString("yyyy-MM-dd HH:mm:ss")));
+
+        
+        StartCoroutine(Upload(formData));
+        
+
+    }
     IEnumerator Start()
     {
         UnityWebRequest www = UnityWebRequest.Get("https://citmalumnes.upc.es/~jial/login.php");
@@ -20,5 +57,21 @@ public class Connect : MonoBehaviour
         }
     }
 
+    IEnumerator Upload(List<IMultipartFormSection> formData) 
+        {
+            UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~jial/uploadData.php", formData);
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+                Debug.Log("Respuesta del PHP: " + www.downloadHandler.text);
+        }
+    }
 
 }
